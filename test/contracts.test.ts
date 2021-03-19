7ddd0392ab5bdf4d5ec5d78613f81d29ba8ddc52
danilo neves cruz <cruzdanilo@gmail.com>
2021-03-19 15:53:48 -0300

    contracts: add wallet to test

--- test/contracts.test.ts
+++ test/contracts.test.ts
@@ -1,13 +1,17 @@
 import 'dotenv/config';
 import { expect } from 'chai';
-import { Contract, ContractFactory } from 'ethers';
-import { ethers } from 'hardhat';
+import { Contract, ContractFactory, Wallet } from 'ethers';
+import { ethers, waffle } from 'hardhat';
 
 describe('bicho', () => {
   let Bicho: ContractFactory;
   let bicho: Contract;
+  let wallet: Wallet;
 
-  before(async () => { Bicho = await ethers.getContractFactory('Bicho'); });
+  before(async () => {
+    Bicho = await ethers.getContractFactory('Bicho');
+    [wallet] = await waffle.provider.getWallets();
+  });
 
   beforeEach(async () => {
     bicho = await Bicho.deploy();
