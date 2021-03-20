946c5488471f970b8159fb43d3e212c94c6fb91e
darto <dalton.oliveira.84@gmail.com>
2021-03-20 00:04:55 -0300

    dropped round on contract; redeem and paybak winners

--- contracts/Bicho.sol
+++ contracts/Bicho.sol
@@ -7,8 +7,7 @@ contract Bicho is ERC1155('https://bicho-nft.web.app/bicho/{id}.json') {
   struct Ticket {
     address player;
     uint8 number;
-    uint128 amount;
-    uint round;
+    uint256 amount;
     uint256 wonShare;
   }
   uint8 constant private BLOCK_INTERVAL = 20;
@@ -16,20 +15,15 @@ contract Bicho is ERC1155('https://bicho-nft.web.app/bicho/{id}.json') {
   uint private currentRound = 0;
   uint private startedOnBlock;
   Ticket[] public tickets;
-  uint128[MAX_NUMS] public sumsByNumber;
+  Ticket[] public redeemList;
+  uint256[MAX_NUMS] public sumsByNumber;
   mapping (uint => uint8) public luckyNumbers;
   event Birth(uint number);
 
-  function breed() public {
-    uint number = uint(blockhash(block.number - 1));
-    _mint(msg.sender, number, 1, '');
-    emit Birth(number);
-  }
-
-  function bet(uint8 number, uint128 amount) public {
+  function bet(uint8 number) public payable {
     if (startedOnBlock == 0 || block.number - startedOnBlock > BLOCK_INTERVAL) endGame();
-    tickets.push(Ticket({ player: msg.sender, number: number, amount: amount, round: currentRound, wonShare: 0 }));
-    sumsByNumber[number] += amount;
+    tickets.push(Ticket({ player: msg.sender, number: number, amount: msg.value, wonShare: 0 }));
+    sumsByNumber[number] += msg.value;
   }
 
   function endGame() public {
@@ -41,22 +35,33 @@ contract Bicho is ERC1155('https://bicho-nft.web.app/bicho/{id}.json') {
       if (number != luckyNumber) totalLost += sumsByNumber[number];
       sumsByNumber[number] = 0;
     }
-    if (totalWon > 0) {
+    if (totalWon > 0 && totalLost > 0) {
       for (uint i = 0; i < tickets.length; i++) {
         Ticket memory ticket = tickets[i];
-        if (ticket.round != currentRound || tickets[i].number != luckyNumber) continue;
-        tickets[i].wonShare = (totalLost * ticket.amount * 9000) / (totalWon * 10000);
+        if (tickets[i].number != luckyNumber) continue;
+        uint256 weight = ticket.amount / totalWon;
+        uint256 withFee = totalLost * 9000 / 10000;
+        ticket.wonShare = withFee * weight;
+        redeemList.push(ticket);
       }
     }
+    while (tickets.length > 0) tickets.pop();
     startedOnBlock = block.number;
     currentRound++;
   }
 
-  function batchRedeem() public {
-    while (tickets.length > 0) {
-      // Ticket memory ticket = tickets[0];
-      // redeem
-      tickets.pop();
+  function redeem() public {
+    for (uint i = 0; i < redeemList.length; i++) {
+      Ticket memory ticket = redeemList[i];
+      breed(ticket.number);
+      console.log(ticket.number, ticket.wonShare, address(this).balance);
+      payable(ticket.player).transfer(ticket.wonShare + ticket.amount);
     }
+    while (redeemList.length > 0) redeemList.pop();
+  }
+
+  function breed(uint8 number) internal {
+    _mint(msg.sender, number, 1, '');
+    emit Birth(number);
   }
 }
