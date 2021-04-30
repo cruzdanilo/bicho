7ba084946b9e440cc8448a83e14756734c42fd20
danilo neves cruz <cruzdanilo@gmail.com>
2021-04-29 23:55:49 -0300

    build: shim dcl globals

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { Configuration, DefinePlugin } from 'webpack';
+import { Configuration, DefinePlugin, ProvidePlugin } from 'webpack';
 
 const {
   BICHO_ADDRESS = '0x9790a13073521575b145360A27fa49943ffF548B',
@@ -22,6 +22,31 @@ function config(_: any, { mode = 'production' }): Configuration {
       new DefinePlugin(Object.fromEntries(Object.entries({
         BICHO_ADDRESS,
       }).map(([k, v]) => [k, JSON.stringify(v)]))),
+      new ProvidePlugin({
+        ActionButton: ['decentraland-ecs', 'ActionButton'],
+        AnimationState: ['decentraland-ecs', 'AnimationState'],
+        Animator: ['decentraland-ecs', 'Animator'],
+        Attachable: ['decentraland-ecs', 'Attachable'],
+        Camera: ['decentraland-ecs', 'Camera'],
+        Color4: ['decentraland-ecs', 'Color4'],
+        Component: ['decentraland-ecs', 'Component'],
+        Entity: ['decentraland-ecs', 'Entity'],
+        Font: ['decentraland-ecs', 'Font'],
+        Fonts: ['decentraland-ecs', 'Fonts'],
+        GLTFShape: ['decentraland-ecs', 'GLTFShape'],
+        Input: ['decentraland-ecs', 'Input'],
+        OnClick: ['decentraland-ecs', 'OnClick'],
+        OnPointerDown: ['decentraland-ecs', 'OnPointerDown'],
+        Quaternion: ['decentraland-ecs', 'Quaternion'],
+        Texture: ['decentraland-ecs', 'Texture'],
+        Transform: ['decentraland-ecs', 'Transform'],
+        UICanvas: ['decentraland-ecs', 'UICanvas'],
+        UIContainerRect: ['decentraland-ecs', 'UIContainerRect'],
+        UIImage: ['decentraland-ecs', 'UIImage'],
+        UIText: ['decentraland-ecs', 'UIText'],
+        Vector3: ['decentraland-ecs', 'Vector3'],
+        engine: ['decentraland-ecs', 'engine'],
+      }),
     ],
   };
 }
