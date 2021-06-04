c7b1729e6bbee54f7b3daf92578e0aae736eb700
danilo neves cruz <cruzdanilo@gmail.com>
2021-06-03 23:46:45 -0300

    use static network detection

--- ecs/web3/getProvider.ts
+++ ecs/web3/getProvider.ts
@@ -1,5 +1,5 @@
 import { DecentralandInterface } from 'decentraland-ecs';
-import { Web3Provider } from '@ethersproject/providers';
+import { StaticJsonRpcProvider, Web3Provider } from '@ethersproject/providers';
 
 declare const dcl: DecentralandInterface;
 
@@ -7,7 +7,9 @@ let provider: Promise<Web3Provider>;
 
 const getProvider = async () => {
   const { rpcHandle } = await dcl.loadModule('web3-provider');
-  return new Web3Provider(await dcl.callRpc(rpcHandle, 'getProvider', []));
+  const web3Provider = new Web3Provider(await dcl.callRpc(rpcHandle, 'getProvider', []));
+  web3Provider.detectNetwork = StaticJsonRpcProvider.prototype.detectNetwork;
+  return web3Provider;
 };
 
 export default async () => {
